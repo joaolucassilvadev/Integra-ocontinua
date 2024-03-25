a564b7817abcb0ab24f0af0dedccaf402537e88b
joaolucassilvadev <jls.silva@discente.ufma.br>
2024-03-25 15:04:37 -0300

    new ci

--- math_test.go
+++ math_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestSoma(t *testing.T) {
 	total := Soma(15, 15)
 
-	if total != 33 {
+	if total != 30 {
 		t.Errorf("resultado da soma é invalido: Resultado %d e o valor esperado era:%d", total, 30)
 	}
 }
